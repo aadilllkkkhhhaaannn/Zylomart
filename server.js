a7a7ee7daebd99e5745aeae51edc7a3f55407e88
AadilKhan0461 <aadilkhan04610461@gmail.com>
2025-04-03 17:05:18 +0530

    use

--- server.js
+++ server.js
@@ -44,6 +44,23 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Cache-Control, Expires, Pragma"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
+
 app.get("/", (req, res) => {
   res.json({
     msg: "welcome to the Zylomart",
