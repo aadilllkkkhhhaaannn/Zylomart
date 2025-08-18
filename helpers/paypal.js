8e3d05e5800cb24f35bd1f1f25e6186ca87d9cd7
AadilKhan0461 <aadilkhan04610461@gmail.com>
2025-08-18 13:59:45 +0530

    mode

--- helpers/paypal.js
+++ helpers/paypal.js
@@ -1,11 +1,9 @@
 const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
-  mode: "sandbox",
-  client_id:
-    "AXc3xhF9JEm4F3Jy_W5ZwHbeIfFwrrT1LIHN99OO9mYvt1-X2AZA7-iS_iOxRZflEKOONj-k7lC3BVa0",
-  client_secret:
-    "EOXDf_fF6N66Zfx9V-9DQF7I0BfkUAe3CmgvxRWX6Kn5rr2jcoVtKqYtLBD96WGfFlS4NmJlxaHQKSRz",
+  mode: process.env.PAYPAL_MODE, // paypal mode
+  client_id: process.env.PAYPAL_CLIENT_ID,
+  client_secret: process.env.PAYPAL_CLIENT_SECRET, 
 });
 
 module.exports = paypal;
